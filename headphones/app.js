aba0dcea0f0b405fa570033db3f37ac52f5ec644
James Rotich <rotichjames8@gmail.com>
2025-01-09 20:22:40 +0300

    update css and html

--- headphones/app.js
+++
@@ -1,9 +0,0 @@
-// Select the menu icon and menu items
-const menuIcon = document.getElementById("menuIcon");
-const menuItems = document.getElementById("menuItems");
-
-// Add a click event listener to the menu icon
-menuIcon.addEventListener("click", () => {
-    // Toggle the 'show' class on the menu items
-    menuItems.classList.toggle("show");
-});
